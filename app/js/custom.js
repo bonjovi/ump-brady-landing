d83ae608b6cd9f489ecce94ee9af45701d802db6
Roman Kalashnikov <rk@MacBook-Pro-Roman.local>
2019-03-04 13:25:06 +0300

    Fixed bugs

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,17 +1,5 @@
 $(function() {
 
-    $('.header__button, .map__overlay').on('click', function() {
-        $('.header__button').toggleClass('header__button_active');
-        if($('.header__button').hasClass('header__button_active')) {
-            $('.header__button').text('Свернуть карту');
-            $('.map').css('height', '500px');
-            $('.map__overlay').css('height', '500px');
-        } else {
-            $('.header__button').text('На карте');
-            $('.map').css('height', '100px');
-            $('.map__overlay').css('height', '100px');
-        }
-        
-    });
+    $(".owl-carousel").owlCarousel();
 
 });
