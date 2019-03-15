70a26ca28d29a2645498085a23281d8af55a3fa4
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2019-03-15 18:06:04 +0300

    Added final changes

--- app/js/custom.js
+++ app/js/custom.js
@@ -11,6 +11,8 @@ $(function() {
 
     $(".fancybox").fancybox();
 
+    $('.tooltipster').tooltipster();
+
     if($(window).width() < 767) {
         $('.pictures__text:first-child').insertBefore('.pictures__section:first-child').css('margin','0 0 10px 0').css('padding','20px');
         $('.pictures__text:last-child').insertBefore('.pictures__section:last-child').css('margin','0 0 10px 0').css('padding','20px');
